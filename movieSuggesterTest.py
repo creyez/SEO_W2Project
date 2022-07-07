8246f6e1cd58bd066e57dd7774aa58a1196351fd
gkaranja76 <gwkaranja02@gmail.com>
2022-07-07 13:36:08 +0000

    Progress
    Co-authored-by: Christian Reyes <chris.reye1@gmail.com>

--- movieSuggesterTest.py
+++ movieSuggesterTest.py
@@ -1,8 +1,9 @@
 import unittest
+from unittest.mock import patch
 import requests
 from movieSuggester import getGenre, getUserRating, getStreamingServices, getMovies, displayMovie
 
-class TestFileName(unittest.TestCase):
+class TestMovieSuggester(unittest.TestCase):
     # def setUp(self):
     #     self.tmdbKey = "37909ab2a58f4d635646887a974c77a1"
 
@@ -15,16 +16,17 @@ class TestFileName(unittest.TestCase):
 
         id_returned = getGenre()
         if type(id_returned) == list:
-            self.assertEqal(len(id_returned) > 0)
+            self.assertNotEqual(len(id_returned), 0)
 
     def test_getUserRating(self):
         # Checking that an exception is raised if the user inputs a value 
         # that cannot be converted into a float
 
-        with self.assertRaises(ValueError) as exception_context:
+        # with self.assertRaises(ValueError) as exception_context:
 
 
-        self.assertRaises()
+        # self.assertRaises()
+        pass
 
 
     def test_getStreamingServices(self):
@@ -33,25 +35,25 @@ class TestFileName(unittest.TestCase):
 
         user_choice = getStreamingServices() 
         if type(user_choice) == list:
-            self.assertEqual(len(user_choice) > 0)
+            self.assertNotEqual(len(user_choice), 0)
 
 
-    # def test_getMovies(self):
-    #     # Testing the APIs response to make sure its not invalid and to make sure the json 
-    #     # response is a list of dictionaries
-    #     fake_json = [{'some':'something'}]
+    def test_getMovies(self):
+        # Testing the APIs response to make sure its not invalid and to make sure the json 
+        # response is a list of dictionaries
+        fake_json = {'some':'something'}
 
-    #     with patch('movieSuggester.getMovies') as mock_get:
-    #         mock_get.return_value.status_code = 200
-    #         mock_get.return_value.json().return_value = fake_json
-    #         response = getMovies.get()
+        with patch('movieSuggester.getMovies') as mock_get:
+            mock_get.return_value.status_code = 200
+            mock_get.return_value.json.return_value = fake_json
+            response = getMovies(genre="", userRating="", streamingServices="")
 
-    #     self.assertEqal(response.status_code, 200)
-    #     self.assertEqal(response.json(), fake_json)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), fake_json)
 
         
     def test_displayMovie(self):
-          # Testing the APIs response to make sure its not invalid and to make sure the json 
+        # Testing the APIs response to make sure its not invalid and to make sure the json 
         # response is a list of dictionaries
         pass
 
