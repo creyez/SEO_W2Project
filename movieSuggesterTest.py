cccad199a1fed1f1a2d0f7e631b0e54e6d186ebd
gkaranja76 <gwkaranja02@gmail.com>
2022-07-07 18:34:54 +0000

    progress
    Co-authored-by: Christian Reyes <chris.reye1@gmail.com>

--- movieSuggesterTest.py
+++ movieSuggesterTest.py
@@ -2,48 +2,33 @@ import unittest
 from movieSuggester import getGenre, getUserRating, checkUserRating, getStreamingServices, getMovies, displayMovie
 
 class TestFileName(unittest.TestCase):
-    # def setUp(self):
-    #     self.tmdbKey = "37909ab2a58f4d635646887a974c77a1"
-
-    # def tearDown(self):
-    #     self.tmdbKey = ''
-
     def test_getGenre(self):
         # Testing the APIs response to make sure its not invalid and to 
         # make sure the genreList and idList are not empty
-
         id_returned = getGenre()
         if type(id_returned) == list:
             self.assertNotEqual(len(id_returned), 0)
 
-    def test_checkUserRating(self):
-        # Checking that an exception is raised if the user inputs a value 
-        # that cannot be converted into a float
-        rating = 'abcd'
-
-        with self.assertRaises(ValueError) as exception_context:
-            checkUserRating(rating)
-        self.assertRaises(
-                   str(exception_context.exception),
-            "Error: input should be a number.", end=" "
-        )
-
-
+    def test_getUserRating(self):
+        # Checking that getUserRating only returns a rating
+        # between 0 and 10 and is a float
+        rating = getUserRating()
+        if rating != '':
+            self.assertEqual(type(rating), float)
+            self.asserTrue(0 <= rating <= 10)
+         
     def test_getStreamingServices(self):
         # Checking that if the user decided to enter a streaming services 
         # the list being returned is not empty
-
         user_choice = getStreamingServices() 
         if type(user_choice) == list:
             self.assertNotEqual(len(user_choice), 0)
-
 
     def test_getMovies(self):
         # Testing the APIs response to make sure its a python dictionary
         response = getMovies(genre="", userRating="", streamingServices="")
         self.assertEqual(type(response), dict)
-
-        
+ 
     def test_displayMovie(self):
         # Testing the APIs response to make sure its not invalid and to make sure the json 
         # response is a list of dictionaries
